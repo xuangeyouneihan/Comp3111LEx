c1799911141355a7a5348463c3b518016dff1c49
xuangeyouneihan <xuangeyouneihan@sina.com>
2025-09-19 19:06:36 +0800

    Add another print line

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -9,6 +9,7 @@ System.out.println("Program ..Starting...");
 System.out.println("1 + 1 = "+2);
 System.out.println(b+" to power "+n+" = "+ myLibrary.Power(b,n));
 System.out.println(n + "! = " + myLibrary.factorial(n));
+System.out.println(1919810);
 System.out.println("Program ..Ended …");
 }
 }
